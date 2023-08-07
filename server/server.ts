7ac5ad5dc966630d581b2a901d9722db44d4da0c
Zachary Lee <zachary@allenlee.dev>
2023-08-07 13:01:06 -0400

    Fixed cors, redeploying server

--- server/server.ts
+++ server/server.ts
@@ -8,25 +8,6 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-var allowlist = [
-  "http://localhost:3000",
-  "https://diamond-gallery-6ibg.vercel.app/",
-];
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header("Origin")) !== -1) {
-    corsOptions = { origin: true, optionsSuccessStatus: 200 };
-  } else {
-    corsOptions = { origin: false, optionsSuccessStatus: 200 };
-  }
-  callback(null, corsOptions);
-};
-
-// MIDDLEWARE
-app.use(express.json());
-// app.use(express.urlencoded({ extended: true }))
-app.use(cors(corsOptionsDelegate));
-
 app.use(express.json());
 app.use(bodyParser.raw({ type: "image/*", limit: "10mb" }));
 
